e445a09a70372ee1883782581427abbcec1f5ee2
Joaquin Cancino <cancinojoaquin12@gmail.com>
2024-09-15 00:18:03 +0000

    a lil correction in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    theme: {
-      extend: {
-        colors: {
-          primary: {
-            DEFAULT: '#1E3A8A', // Azul oscuro profesional
-            50: '#EFF6FF',
-            100: '#DBEAFE',
-            200: '#BFDBFE',
-            300: '#93C5FD',
-            400: '#60A5FA',
-            500: '#3B82F6',
-            600: '#2563EB',
-            700: '#1D4ED8',
-            800: '#1E40AF',
-            900: '#1E3A8A',
-          },
-          accent: {
-            DEFAULT: '#1D4ED8', // Un azul ligeramente más claro para acentos
-          },
-          background: '#F8FAFC', // Color de fondo suave y neutral
-          card: '#FFFFFF', // Fondo de tarjetas y secciones
-          foreground: '#111827', // Color para el texto principal
-        },
-        fontFamily: {
-          sans: ['Inter', 'sans-serif'], // Tipografía moderna
-        },
-      },
-    },
-  };
-  
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
