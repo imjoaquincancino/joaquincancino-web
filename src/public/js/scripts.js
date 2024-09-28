5739c24e02c2385231a932889824e961c53bbb59
Joaquin Cancino <cancinojoaquin12@gmail.com>
2024-09-28 18:45:06 +0000

    partials changes

--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -1,3 +1,20 @@
+const menuBtn = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuBtn.addEventListener('click', () => {
+    if (mobileMenu.classList.contains('hidden')) {
+        mobileMenu.classList.remove('hidden');
+        mobileMenu.classList.remove('animate__slideOutUp');
+        mobileMenu.classList.add('animate__animated', 'animate__slideInDown');
+    } else {
+        mobileMenu.classList.remove('animate__slideInDown');
+        mobileMenu.classList.add('animate__slideOutUp');
+        setTimeout(() => {
+            mobileMenu.classList.add('hidden');
+        }, 500);
+    }
+});
+
 document.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
     const welcomeBackground = document.querySelector(".welcome-background");
@@ -5,20 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     welcomeBackground.style.filter = "blur(5px)";
     welcomeContent.style.opacity = "1";
-  }, 1000); // Espera 2 segundos antes de aplicar el efecto
-});
-
-const navbar = document.getElementById("navbar");
-navbar.classList.add("bg-opacity-10");
-navbar.classList.add("fixed");
-window.addEventListener("scroll", function () {
-  if (window.scrollY > 515) {
-    navbar.classList.remove("bg-opacity-10");
-    navbar.classList.add("bg-opacity-100");
-  } else {
-    navbar.classList.add("bg-opacity-10");
-    navbar.classList.remove("bg-opacity-100");
-  }
+  }, 1000);
 });
 
 window.onscroll = function () {
@@ -31,10 +35,10 @@ function showScrollTopBtn() {
     document.body.scrollTop > 100 ||
     document.documentElement.scrollTop > 100
   ) {
-    scrollTopBtn.classList.remove("hidden"); // Mostrar el botón
-    scrollTopBtn.classList.add("block"); // Asegurar que sea visible
+    scrollTopBtn.classList.remove("hidden");
+    scrollTopBtn.classList.add("block");
   } else {
-    scrollTopBtn.classList.add("hidden"); // Ocultar el botón cuando esté al inicio
+    scrollTopBtn.classList.add("hidden");
   }
 }
 
@@ -47,7 +51,6 @@ function toggleMaintenance(button) {
   const icon = button.querySelector('i');
   const list = button.nextElementSibling;
 
-  // Cambiar el ícono y alternar la visibilidad del contenido
   list.classList.toggle('hidden');
   if (list.classList.contains('hidden')) {
     icon.classList.remove('fa-caret-up');
